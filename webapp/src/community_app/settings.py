e9e98026b518a69e5b9f38c16b33a2a5404d4d01
Aviraj Singh <avi.singh@bighealth.com>
2020-11-20 15:48:03 -0500

    Revert "add instrumentation"
    This reverts commit b2bc6b7cb38e3942c93d00b36f4a07909c54d276.

--- webapp/src/community_app/settings.py
+++ webapp/src/community_app/settings.py
@@ -259,7 +259,6 @@ MIDDLEWARE = [
     "misago.users.middleware.OnlineTrackerMiddleware",
     "misago.admin.middleware.AdminAuthMiddleware",
     "misago.threads.middleware.UnreadThreadsCountMiddleware",
-    "pyinstrument.middleware.ProfilerMiddleware",
 ]
 
 ROOT_URLCONF = "community_app.urls"
