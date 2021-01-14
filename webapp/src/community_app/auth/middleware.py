8a6e563377a24b23279149f2d2c1faec2732413c
Will Bastian <will.bastian@bighealth.com>
2021-01-14 11:08:50 -0500

    WIP session managment and login flow support

--- webapp/src/community_app/auth/middleware.py
+++ webapp/src/community_app/auth/middleware.py
@@ -1,17 +1,17 @@
 import logging
 
 from bh.core_utils.bh_exception import BHException
+# from bh.core_utils.request_context import RequestContext, CALL_ID, HEADERS
 from bh.services.factory import Factory
-from bh_settings import get_settings
+# from bh_settings import get_settings
 
 
-from community_app.auth.pipeline import extract_tokens
+# from community_app.auth.pipeline import extract_tokens
 
 from django.conf import settings
 from django.contrib.auth import logout
-from django.shortcuts import redirect
+# from django.shortcuts import redirect
 from misago.users.models import AnonymousUser
-from django.utils.deprecation import MiddlewareMixin
 
 
 logger = logging.getLogger("CommunityMiddleware")
@@ -25,42 +25,80 @@ def platgen_session_middleware(get_response):
     ACCESS_TOKEN = "access_token"
     REFRESH_TOKEN = "refresh_token"
 
-    def middleware(request):
+    def extract_tokens(request, *args, **kwargs) -> dict:
+        return dict(access_token=request.COOKIES.get("access_token"), refresh_token=request.COOKIES.get("refresh_token"))
+
+    def middleware(request, *args, **kwargs):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
         cookies_updated = False
 
         # Here we validate existence of an authentication entity but ONLY for non-admin users.
+        # We must support admin users logging in with email, and not using Sleepio auth cookies.
         # If we don't have an authentication entity, OR we don't have an access_token (e.g. it's expired)
-        # we attempt to refresh the tokens with refresh_access_token
+        # we attempt to refresh the tokens with refresh_token
         #
         # If that fails, we log the user out and redirect to sleepio
         # If that suceeds, we update the tokens
-        if settings.SESSION_COOKIE_NAME in request.COOKIES and not request.user.is_superuser:
+        #
+        # It's important that this is placed after Misago's user middleware items, to ensure we have a
+        # user attached to the session, which allows us to log the user off prior to redirecting.
+        #
+        # This is to avoid managing sessions directly, and using built-in behavior to log a user out.
+        # If we have an authentication entity, we enrich the request to include the platform_user_id, which is used in
+        # the social_auth_pipeline, fetch_user_account
+
+        if not request.user.is_superuser:
             tokens = extract_tokens(request)
             access_token = tokens.get(ACCESS_TOKEN)
             refresh_token = tokens.get(REFRESH_TOKEN)
             authentication_service = Factory.create("UserAccountAuthentication", "1")
             authentication_entity = None
             if access_token or refresh_token:
+                # TODO question, this works with either token. If we don't have a refresh token, this means we'll be redirected
+                # when it times out. Problem?
                 authentication_entity = authentication_service.find_with_tokens(access_token=access_token, refresh_token=refresh_token)
             if access_token is None or not authentication_entity:
                 try:
+                    # TODO POC on calling via ClientGateway to leverage its cookie creation
+                    # TBD: How to parse response?
+                    # client_gateway_service = Factory.create("ClientGateway", "1")
+                    # result = None
+                    # with RequestContext() as rc:
+                    #     rc.attach(HEADERS, {"Cookie": f"refresh_token={refresh_token}"})
+                    #     result = client_gateway_service.proxy_service_call(
+                    #         service_name="UserAccountAuthentication",
+                    #         service_version="1",
+                    #         service_method="refresh_access_token",
+                    #     )
+                    #     logger.info(result)
+                    #     logger.info(rc)
+                    # logger.info(result)
+
                     tokens = authentication_service.refresh_access_token(refresh_token=refresh_token)
                     access_token, refresh_token = tokens["access_token"], tokens["refresh_token"]
+                    authentication_entity = authentication_service.find_with_tokens(access_token=access_token, refresh_token=refresh_token)
                     cookies_updated = True
                 except BHException as e:
-                    logger.debug(e)
-                    logout(request)
-                    request.user = AnonymousUser()
-                    return redirect(get_settings("sleepio_app_url"))
+                    logger.info(e)
+                    if settings.SESSION_COOKIE_NAME in request.COOKIES:
+                        logout(request)
+                        request.user = AnonymousUser()
+                    # TODO enable this when we have sleepio redirect URLS,
+                    #   and second level domain cookies working.
+                    #   Until then, this will always fire upon landing on the page
+                    #   because we won't have the cookies generated
+                    # return redirect(get_settings("sleepio_app_url"))
 
+        if authentication_entity:
+            request._platform_user_id = authentication_entity.get("user_id")
         response = get_response(request)
 
         # Code to be executed for each request/response after
         # the view is called.
 
+        # TODO make consistent with Sleepio
         if cookies_updated:
             response.set_cookie(
                 ACCESS_TOKEN,
