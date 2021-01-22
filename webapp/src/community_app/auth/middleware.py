199ffc706187c91e0b4a89873158270bf50af65d
Will Bastian <will.bastian@sleepio.com>
2021-01-22 15:20:53 -0500

    cookie generation consistent with sleepio

--- webapp/src/community_app/auth/middleware.py
+++ webapp/src/community_app/auth/middleware.py
@@ -1,23 +1,22 @@
 import logging
 
-from bh.core_utils.bh_exception import BHException
-# from bh.core_utils.request_context import RequestContext, CALL_ID, HEADERS
-from bh.services.factory import Factory
-# from bh_settings import get_settings
-
-
-# from community_app.auth.pipeline import extract_tokens
-
+from datetime import datetime, timedelta
 from django.conf import settings
 from django.contrib.auth import logout
+
 # from django.shortcuts import redirect
 from misago.users.models import AnonymousUser
+from typing import Optional
+
+from bh.core_utils.bh_exception import BHException
+from bh.services.factory import Factory
+from bh_settings import get_settings
 
 
 logger = logging.getLogger("CommunityMiddleware")
 
-# import debugpy
-# debugpy.listen(("0.0.0.0", 8211))
+#import debugpy
+#debugpy.listen(("0.0.0.0", 8211))
 
 
 def platgen_session_middleware(get_response):
@@ -27,6 +26,21 @@ def platgen_session_middleware(get_response):
 
     def extract_tokens(request, *args, **kwargs) -> dict:
         return dict(access_token=request.COOKIES.get(ACCESS_TOKEN), refresh_token=request.COOKIES.get(REFRESH_TOKEN))
+
+    def construct_cookie_domain_from_request_headers(request_headers: dict) -> Optional[str]:
+        """Construct a domain string for the cookies from request headers (specifically the Origin header).
+
+        Args:
+            request_headers (dict): the request headers
+
+        Returns:
+            Optional[str]: the domain string
+        """
+        try:
+            domain = ".".join(request_headers["host"].split(".")[1:])
+            return "." + domain if domain else None
+        except KeyError:
+            return None
 
     def middleware(request, *args, **kwargs):
         # Code to be executed for each request before
@@ -52,8 +66,7 @@ def platgen_session_middleware(get_response):
 
         if not request.user.is_superuser:
             tokens = extract_tokens(request)
-            access_token = tokens.get(ACCESS_TOKEN)
-            refresh_token = tokens.get(REFRESH_TOKEN)
+            access_token, refresh_token = tokens.get(ACCESS_TOKEN), tokens.get(REFRESH_TOKEN)
             authentication_service = Factory.create("UserAccountAuthentication", "1")
             if access_token or refresh_token:
                 # TODO question, this works with either token. If we don't have a refresh token, this means we'll be redirected
@@ -62,7 +75,7 @@ def platgen_session_middleware(get_response):
             if access_token is None or not authentication_entity:
                 try:
                     tokens = authentication_service.refresh_access_token(refresh_token=refresh_token)
-                    access_token, refresh_token = tokens["access_token"], tokens["refresh_token"]
+                    access_token, refresh_token = tokens.get(ACCESS_TOKEN), tokens.get(REFRESH_TOKEN)
                     authentication_entity = authentication_service.find_with_tokens(access_token=access_token, refresh_token=refresh_token)
                     cookies_updated = True
                 except BHException as e:
@@ -83,20 +96,29 @@ def platgen_session_middleware(get_response):
         # Code to be executed for each request/response after
         # the view is called.
 
-        # TODO make consistent with Sleepio
         if cookies_updated:
+            clear_cookie_dt = datetime(year=1970, month=1, day=1)
+            domain = construct_cookie_domain_from_request_headers(request.headers)
             response.set_cookie(
                 ACCESS_TOKEN,
                 access_token,
-                secure=settings.SESSION_COOKIE_SECURE or None,
-                httponly=settings.SESSION_COOKIE_HTTPONLY or None,
+                expires=(datetime.utcnow() + timedelta(seconds=get_settings("access_token_cookie_expiration_seconds")))
+                if access_token
+                else clear_cookie_dt,
+                domain=domain,
+                secure=get_settings("secure_cookies", True),
+                httponly=True,
             )
 
             response.set_cookie(
                 REFRESH_TOKEN,
                 refresh_token,
-                secure=settings.SESSION_COOKIE_SECURE or None,
-                httponly=settings.SESSION_COOKIE_HTTPONLY or None,
+                expires=(datetime.utcnow() + timedelta(days=get_settings("refresh_token_cookie_expiration_days")))
+                if refresh_token
+                else clear_cookie_dt,
+                secure=get_settings("secure_cookies", True),
+                domain=domain,
+                httponly=True,
             )
 
         return response
